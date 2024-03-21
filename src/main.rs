cbe23fd96fe611a461433117a5e5115f4c2064f9
Emerentius <emerentius@arcor.de>
2024-03-21 14:15:03 +0100

    add naming of stashes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{fs::Metadata, path::PathBuf};
+use std::{fs::Metadata, str::FromStr};
 
+use camino::{Utf8Path as Path, Utf8PathBuf as PathBuf};
 use clap::Parser;
 use directories::ProjectDirs;
 use eyre::{eyre, Result};
@@ -15,14 +16,30 @@ struct Args {
 #[derive(clap::Subcommand, Debug)]
 enum Subcommand {
     List,
-    Push,
-    Show { index: Option<u32> },
-    Pop,
+    Push { name: Option<String> },
+    Show { stash_id: Option<StashId> },
+    Pop { name: Option<String> },
     Clear,
 }
 
+#[derive(Debug)]
+struct StashId {
+    name: String,
+    index: Option<u32>,
+}
+
+impl FromStr for StashId {
+    type Err = eyre::Report;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        let (name, index) = Data::parse_id_arg(id)?;
+        Ok(StashId { name, index })
+    }
+}
+
 struct Data {
-    path: PathBuf,
+    name: String,
+    index: u32,
     metadata: Metadata,
 }
 
@@ -33,8 +50,14 @@ impl Data {
             .fs_err_read_dir()?
             .map(|entry| {
                 let entry = entry?;
+                let path = PathBuf::from_path_buf(entry.path()).unwrap();
+                let filename = path.file_name().unwrap();
+
+                let (name, index) = Self::parse_id_internal(filename)?;
+
                 Ok(Data {
-                    path: entry.path(),
+                    name,
+                    index,
                     metadata: entry.metadata()?,
                 })
             })
@@ -50,12 +73,51 @@ impl Data {
         Ok(stashes)
     }
 
-    fn get_newest(proj_dir: &ProjectDirs) -> Result<Option<Data>> {
-        Ok(Self::detect(proj_dir)?.into_iter().next())
+    fn detect_named(proj_dir: &ProjectDirs, name: &str) -> Result<Vec<Data>> {
+        let mut stashes = Self::detect(proj_dir)?;
+        stashes.retain(|st| st.name == name);
+        Ok(stashes)
     }
 
-    fn get(proj_dir: &ProjectDirs, idx: usize) -> Result<Option<Data>> {
-        Ok(Self::detect(proj_dir)?.into_iter().nth(idx))
+    fn parse_id_internal(stash_id: &str) -> Result<(String, u32)> {
+        Self::_parse_id(stash_id, false).map(|(name, id)| (name, id.unwrap()))
+    }
+
+    fn parse_id_arg(stash_id: &str) -> Result<(String, Option<u32>)> {
+        Self::_parse_id(stash_id, false)
+    }
+
+    fn _parse_id(stash_id: &str, internal: bool) -> Result<(String, Option<u32>)> {
+        let separator = if internal { '_' } else { ':' };
+        // TODO: need to filter out invalid characters
+        match stash_id.rsplit_once(separator) {
+            Some((name, index)) => {
+                let index = index.parse().map_err(|_| {
+                    eyre!("couldn't parse stash id. Expected <name>.<number>, got: {stash_id}")
+                })?;
+                Ok((name.to_owned(), Some(index)))
+            }
+            None => Ok((stash_id.to_owned(), None)),
+        }
+    }
+
+    fn get_newest(proj_dir: &ProjectDirs, name: &str) -> Result<Option<Data>> {
+        Self::get(proj_dir, name, 0)
+    }
+
+    fn get(proj_dir: &ProjectDirs, name: &str, idx: usize) -> Result<Option<Data>> {
+        Ok(Self::detect_named(proj_dir, name)?.into_iter().nth(idx))
+    }
+
+    // this is also the internal stash_id
+    fn filename(&self) -> String {
+        format!("{}_{}", self.name, self.index)
+    }
+
+    fn path(&self, proj_dir: &ProjectDirs) -> PathBuf {
+        Path::from_path(proj_dir.data_dir())
+            .unwrap()
+            .join(self.filename())
     }
 }
 
@@ -66,7 +128,7 @@ fn main() -> Result<()> {
         .ok_or_else(|| eyre!("couldn't get project dirs"))?;
     fs_err::create_dir_all(proj_dirs.data_dir())?;
 
-    match args.command.unwrap_or(Subcommand::Push) {
+    match args.command.unwrap_or(Subcommand::Push { name: None }) {
         Subcommand::List => {
             let stashes = Data::detect(&proj_dirs)?;
             for stash in stashes.into_iter().rev() {
@@ -77,38 +139,35 @@ fn main() -> Result<()> {
                 // TODO: use better time format
                 let stash_time =
                     stash_time.format(&time::format_description::well_known::Rfc3339)?;
-                println!(
-                    "{}: {}",
-                    stash.path.file_name().unwrap().to_str().unwrap(),
-                    stash_time
-                );
+                println!("{}: {}", stash.filename(), stash_time);
             }
         }
-        Subcommand::Push => {
-            let last_number = Data::get_newest(&proj_dirs)?;
-            let next_number = last_number.map_or(0, |last| {
-                last.path
-                    .file_name()
-                    .unwrap()
-                    .to_str()
-                    .unwrap()
-                    .parse::<u32>()
-                    .unwrap()
-                    + 1
-            });
-            let mut file =
-                fs_err::File::create(proj_dirs.data_dir().join(next_number.to_string()))?;
+        Subcommand::Push { name } => {
+            let name = name.unwrap_or(String::new());
+            let prev_stash = Data::get_newest(&proj_dirs, &name)?;
+            let next_idx = prev_stash.map_or(0, |st| st.index + 1);
+            let filename = format!("{name}.{next_idx}");
+            let mut file = fs_err::File::create(proj_dirs.data_dir().join(filename))?;
             std::io::copy(&mut std::io::stdin().lock(), &mut file)?;
         }
-        Subcommand::Show { index } => {
-            let desired_stash = Data::get(&proj_dirs, index.unwrap_or(0) as usize)?;
-            print_stash(desired_stash.as_ref())?;
+        Subcommand::Show { stash_id } => {
+            let (name, index) = match stash_id {
+                Some(StashId { name, index }) => (Some(name), index),
+                None => (None, None),
+            };
+            let desired_stash = Data::get(
+                &proj_dirs,
+                name.as_deref().unwrap_or_default(),
+                index.unwrap_or(0) as usize,
+            )?;
+            print_stash(&proj_dirs, desired_stash.as_ref())?;
         }
-        Subcommand::Pop => {
-            let desired_stash = Data::get_newest(&proj_dirs)?;
-            print_stash(desired_stash.as_ref())?;
+        Subcommand::Pop { name } => {
+            let name = name.as_deref().unwrap_or_default();
+            let desired_stash = Data::get_newest(&proj_dirs, name)?;
+            print_stash(&proj_dirs, desired_stash.as_ref())?;
             if let Some(stash) = desired_stash {
-                fs_err::remove_file(stash.path)?;
+                fs_err::remove_file(stash.path(&proj_dirs))?;
             }
         }
         Subcommand::Clear => {
@@ -121,10 +180,10 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn print_stash(stash: Option<&Data>) -> Result<()> {
+fn print_stash(proj_dir: &ProjectDirs, stash: Option<&Data>) -> Result<()> {
     match stash {
         Some(stash) => {
-            let mut file = fs_err::File::open(&stash.path)?;
+            let mut file = fs_err::File::open(stash.path(proj_dir))?;
             let stdout = std::io::stdout();
             std::io::copy(&mut file, &mut stdout.lock())?;
         }
