45d9376dd19baf0c0c318140fd20b1ba930806df
Emerentius <emerentius@arcor.de>
2024-03-21 15:04:25 +0100

    swap fully to file model from stack model
    identify stashes only by name, there are no more indices

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::Metadata, str::FromStr};
+use std::fs::Metadata;
 
 use camino::{Utf8Path as Path, Utf8PathBuf as PathBuf};
 use clap::Parser;
@@ -10,36 +10,20 @@ use fs_err::PathExt;
 #[clap(author, version, about, long_about = None)]
 struct Args {
     #[clap(subcommand)]
-    command: Option<Subcommand>,
+    command: Subcommand,
 }
 
 #[derive(clap::Subcommand, Debug)]
 enum Subcommand {
     List,
-    Push { name: Option<String> },
-    Show { stash_id: Option<StashId> },
-    Pop { name: Option<String> },
+    Push { name: String },
+    Show { name: String },
+    Pop { name: String },
     Clear,
-}
-
-#[derive(Debug)]
-struct StashId {
-    name: String,
-    index: Option<u32>,
-}
-
-impl FromStr for StashId {
-    type Err = eyre::Report;
-
-    fn from_str(id: &str) -> Result<Self, Self::Err> {
-        let (name, index) = Data::parse_id_arg(id)?;
-        Ok(StashId { name, index })
-    }
 }
 
 struct Data {
     name: String,
-    index: u32,
     metadata: Metadata,
 }
 
@@ -53,11 +37,8 @@ impl Data {
                 let path = PathBuf::from_path_buf(entry.path()).unwrap();
                 let filename = path.file_name().unwrap();
 
-                let (name, index) = Self::parse_id_internal(filename)?;
-
                 Ok(Data {
-                    name,
-                    index,
+                    name: filename.to_owned(),
                     metadata: entry.metadata()?,
                 })
             })
@@ -73,45 +54,25 @@ impl Data {
         Ok(stashes)
     }
 
-    fn detect_named(proj_dir: &ProjectDirs, name: &str) -> Result<Vec<Data>> {
-        let mut stashes = Self::detect(proj_dir)?;
-        stashes.retain(|st| st.name == name);
-        Ok(stashes)
-    }
-
-    fn parse_id_internal(stash_id: &str) -> Result<(String, u32)> {
-        Self::_parse_id(stash_id, false).map(|(name, id)| (name, id.unwrap()))
-    }
-
-    fn parse_id_arg(stash_id: &str) -> Result<(String, Option<u32>)> {
-        Self::_parse_id(stash_id, false)
-    }
-
-    fn _parse_id(stash_id: &str, internal: bool) -> Result<(String, Option<u32>)> {
-        let separator = if internal { '_' } else { ':' };
-        // TODO: need to filter out invalid characters
-        match stash_id.rsplit_once(separator) {
-            Some((name, index)) => {
-                let index = index.parse().map_err(|_| {
-                    eyre!("couldn't parse stash id. Expected <name>.<number>, got: {stash_id}")
-                })?;
-                Ok((name.to_owned(), Some(index)))
-            }
-            None => Ok((stash_id.to_owned(), None)),
-        }
-    }
-
-    fn get_newest(proj_dir: &ProjectDirs, name: &str) -> Result<Option<Data>> {
-        Self::get(proj_dir, name, 0)
-    }
-
-    fn get(proj_dir: &ProjectDirs, name: &str, idx: usize) -> Result<Option<Data>> {
-        Ok(Self::detect_named(proj_dir, name)?.into_iter().nth(idx))
+    fn get(proj_dir: &ProjectDirs, name: &str) -> Result<Option<Data>> {
+        let data_dir = Path::from_path(proj_dir.data_dir())
+            .ok_or(eyre!("non-utf8 data dir path"))?
+            .to_owned();
+        let file = data_dir.join(name);
+        let metadata = match fs_err::metadata(file) {
+            Ok(metadata) => metadata,
+            Err(e) if e.kind() == std::io::ErrorKind::NotFound => return Ok(None),
+            err @ Err(_) => err?,
+        };
+        Ok(Some(Data {
+            name: name.to_owned(),
+            metadata,
+        }))
     }
 
     // this is also the internal stash_id
     fn filename(&self) -> String {
-        format!("{}_{}", self.name, self.index)
+        self.name.to_string()
     }
 
     fn path(&self, proj_dir: &ProjectDirs) -> PathBuf {
@@ -128,7 +89,7 @@ fn main() -> Result<()> {
         .ok_or_else(|| eyre!("couldn't get project dirs"))?;
     fs_err::create_dir_all(proj_dirs.data_dir())?;
 
-    match args.command.unwrap_or(Subcommand::Push { name: None }) {
+    match args.command {
         Subcommand::List => {
             let stashes = Data::detect(&proj_dirs)?;
             for stash in stashes.into_iter().rev() {
@@ -143,28 +104,16 @@ fn main() -> Result<()> {
             }
         }
         Subcommand::Push { name } => {
-            let name = name.unwrap_or(String::new());
-            let prev_stash = Data::get_newest(&proj_dirs, &name)?;
-            let next_idx = prev_stash.map_or(0, |st| st.index + 1);
-            let filename = format!("{name}.{next_idx}");
+            let filename = name.to_string();
             let mut file = fs_err::File::create(proj_dirs.data_dir().join(filename))?;
             std::io::copy(&mut std::io::stdin().lock(), &mut file)?;
         }
-        Subcommand::Show { stash_id } => {
-            let (name, index) = match stash_id {
-                Some(StashId { name, index }) => (Some(name), index),
-                None => (None, None),
-            };
-            let desired_stash = Data::get(
-                &proj_dirs,
-                name.as_deref().unwrap_or_default(),
-                index.unwrap_or(0) as usize,
-            )?;
+        Subcommand::Show { name } => {
+            let desired_stash = Data::get(&proj_dirs, &name)?;
             print_stash(&proj_dirs, desired_stash.as_ref())?;
         }
         Subcommand::Pop { name } => {
-            let name = name.as_deref().unwrap_or_default();
-            let desired_stash = Data::get_newest(&proj_dirs, name)?;
+            let desired_stash = Data::get(&proj_dirs, &name)?;
             print_stash(&proj_dirs, desired_stash.as_ref())?;
             if let Some(stash) = desired_stash {
                 fs_err::remove_file(stash.path(&proj_dirs))?;
